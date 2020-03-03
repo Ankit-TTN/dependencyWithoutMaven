5d5914e6e5700a492590b40c1721369b6c5aa28c
Ankit Kumar <you@example.com>
2020-03-03 21:20:09 +0530

    Dependency Without Maven

---
+++ src/practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+
+public class Main {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        Logger log = Logger.getLogger(Main.class.getName());
+        log.info("hello");
+
+    }
+}
